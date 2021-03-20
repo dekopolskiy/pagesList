5aaa99e277189b50eb0204f3b8ae2a148b1b5f2a
dmitri <dmitridecopolski@yandex.ru>
2021-03-21 00:17:07 +0300

    style

--- src/Paginator/Paginator.js
+++ src/Paginator/Paginator.js
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
 import styles from './Paginator.module.css';
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 
 
 const Paginator = ({ allUsers, usersOnPage }) => {
   //1.calculate all pages, create array of pages
   let allPages = Math.ceil(allUsers / usersOnPage);
   let everyPage = [];
-  for (let i = 1; i <= quantityPages; i++) {
+  for (let i = 1; i <= allPages; i++) {
     everyPage.push(i);
   }
   //2.leftParam and rigtParam for array.slice(left, right) for show portion pages
@@ -18,17 +19,17 @@ const Paginator = ({ allUsers, usersOnPage }) => {
 
   return (
     <div>{/*hide left button if left == 0 OR create button disabled*/}
-      {leftOrRight.l == 0 ? null : <button onClick={() => moveRightOrLeft(-10)}>left</button>}
       <div className={styles.main}>
+        {leftOrRight.l == 0 ? <FaChevronLeft className={styles.chevron}/> : <FaChevronLeft className={styles.chevron} onClick={() => moveRightOrLeft(-10)} />}
+
         {/*Чтобы не перебирать целиком, копируем только необходимую часть при помощи хука  */}
         {everyPage
           .slice(leftOrRight.l, leftOrRight.r)
           .map((i) => <div>{i}</div>)}
+        {leftOrRight.r >= allPages ? <FaChevronRight className={styles.chevron}/> : <FaChevronRight className={styles.chevron} onClick={() => moveRightOrLeft(10)} />}
 
       </div>
       {/*hide right button if right == allPages OR create button disabled*/}
-      {leftOrRight.r >= allPages? null : <button onClick={() => moveRightOrLeft(10)}>right</button>}
-
     </div>
   )
 }
